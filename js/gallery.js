8670c0c398b82de8468643cdef89e3f1ecdd0706
dashiel <dashieluk@gmail.com>
2022-03-22 12:46:40 +0000

    Added new Image object so website preloads images

--- js/gallery.js
+++ js/gallery.js
@@ -2,7 +2,6 @@
 // The main task is to attach an event listener to each image of the gallery
 // and respond appropriately on click.
 
-
 function activateGallery() {
   let thumbnails  = document.querySelectorAll("#gallery-thumbs > div > img" );
   let mainImage   = document.querySelector("#gallery-photo img");
@@ -10,24 +9,27 @@ function activateGallery() {
   // the above is using different bracket notation just to indicate that either works.
 // The ">" notation indicates that the 'img' is "inside" the 'div', which is inside the CSS id #gallery-thumbs
 
-// Image ingo to be updated
-  let galleryInfo = document.querySelector("#gallery-info");
-  let title       = galleryInfo.querySelector(".title");
-  let description = galleryInfo.querySelector(".description");
-
-
     thumbnails.forEach(function(thumbnail) {
-      thumbnail.addEventListener("click", function() {
-         // Set clicked image as main image.
-         let newImageSrc = thumbnail.dataset.largeVersion;
-         let newImageAlt = thumbnail.alt;
+      // Preload large images.
+      let newImageSrc   = thumbnail.dataset.largeVersion;
+      let largeVersion  = new Image();
+      let newImageAlt   = thumbnail.alt;
+      largeVersion.src  = //fill in//
 
+
+         thumbnail.addEventListener("click", function() {
+                // Set clicked image as display image.
         mainImage.setAttribute("src", newImageSrc);
         mainImage.setAttribute("alt", newImageAlt);
 
  // Change which image is current (highlights thumbnail on clicking it).
         document.querySelector(".current").classList.remove("current");
         thumbnail.parentNode.classList.add("current");
+
+        // Image to be updated
+          let galleryInfo = document.querySelector("#gallery-info");
+          let title       = galleryInfo.querySelector(".title");
+          let description = galleryInfo.querySelector(".description");
 
 
         // Update image info.
